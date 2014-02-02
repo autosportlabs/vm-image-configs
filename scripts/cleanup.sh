8b0ed06225bebb3e0dc6913b258ba9ba879b4db0
Brian Lalor <blalor@bravo5.org>
2014-02-02 09:00:26 -0500

    Set system clock from hardware clock to prevent time offset
    Seems like the VBox Guest Additions messed up the clock somehow and
    the superblock on the filesystems ended up with a time many hours in
    the future, which required a manual fsck intervention.  This seems to
    have fixed the problem.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,4 +4,10 @@ set -e
 set -u
 set -x
 
+## set system clock to hardware clock.  I think this might solve the problem I
+## saw where the filesystem superblocks were like a day ahead of the real time
+## after creating a new VirtualBox image.
+date
+hwclock -s
+date
 yum -y clean all
